eeea0643c7e32a3e3f15c5e33ee52afc5b9b6a40
Ankitmaurya-1 <ankitmaurya9560@gmail.com>
2023-12-25 15:25:25 +0530

    Aukat Dikh gyi

---
+++ Video7/index.js
@@ -0,0 +1,33 @@
+// This is for string tutroiral
+// let a = "Ankit Maurya"
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+// console.log(a[4]);
+// console.log(a[5]);
+// console.log(a[6]);
+
+// console.log(a.length);
+
+// let real_name = "Ankit"
+// let name = "Maurya"
+
+// console.log(`His friend first is ${real_name} and his last name is ${name}`);
+
+// 
+let a = 1;
+let b = 5;
+let table;
+while (a < 11){
+    table = a * b
+    console.log(`${a} * ${b} = ${table}`);
+    a++;
+}
+
+
+let fact =1;
+for (let b = 5; b >= 1; b--) {
+    fact = b * fact
+}
+console.log(fact);
