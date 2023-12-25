1edca81627a41f33cb30c707ec5f7e08ff15531a
Ankitmaurya-1 <ankitmaurya9560@gmail.com>
2023-12-25 10:32:40 +0530

    get deeper knowledge about funcitons

--- Video6/script.js
+++ Video6/script.js
@@ -1,26 +1,44 @@
+let a = parseInt(prompt("Enter first number: "));
+let c = prompt("Enter first operator : + , - , * , / : ");
+let b = parseInt(prompt("Enter second number: "));
+
 let random = Math.random()
 console.log(random);
-let num1 = prompt("Enter your first number: ");
-let oper = prompt("Enter your operator from (+,-,*,/,): ");
-let num2 = prompt("Enter your second number: ");
 
-let obj = {
-    "+":"-",
-    "*":"+",
-    "-":"/",
-    "/":"**"
+if (c == '+') {
+    if (random < 0.10) {
+        prompt("Result is:- ", a - b);
+    }
+    else {
+        prompt("Result is:- ", a + b);
+    }
+}
+else if (c == '-') {
+    if (random < 0.10) {
+        prompt("Result is:- ", a + b);
+    }
+    else {
+        prompt("Result is:- ", a - b);
+    }
+}
+else if (c == '*') {
+    if (random < 0.10) {
+        prompt("Result is:- ", a / b);
+    }
+    else {
+        prompt("Result is:- ", a * b);
+    }
+}
+else if (c == '/'){
+    if (random < 0.10) {
+        prompt("Result is:- ", a * b);
+
+    }
+    else {
+        prompt("Result is:- ", a / b);
+    }
+}
+else {
+    alert("Enter valid number or operator")
 }
 
-
-if(random >0.1){
-    // Perfom right calculation
-   alert(`The result is ${eval(`${num1} ${oper} ${num2}`)}`)
-    // prompt("Your result is : ",num1 - num2)
-    // prompt("Your result is : ",num1 * num2)
-    // prompt("Your result is : ",num1 / num2)
-}
-else{
-    // perfom wrong calculation
-    alert(`The result is ${eval(`${num1} ${oper} ${num2}`)}`)
-    oper = obj[oper]
-}
