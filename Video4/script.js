2cbb322dd0240fd837c9feb8096042b101e7348c
Ankitmaurya-1 <ankitmaurya9560@gmail.com>
2023-12-27 15:26:19 +0530

    Just revision bascis

--- Video4/script.js
+++ Video4/script.js
@@ -1,35 +1,35 @@
-let i = 1;
-let q = 2;
-let table;
-for (let i = 1; i < 11; i++) {
-    table = i * q
-    console.log(`${q}*${i} = ${table}`);
-}
+// let i = 1;
+// let q = 2;
+// let table;
+// for (let i = 1; i < 11; i++) {
+//     table = i * q
+//     console.log(`${q}*${i} = ${table}`);
+// }
 
-while loop
-let a = 1;
+// while loop
+// let a = 1;
 
-while (a<=11) {
-    console.log(a);
-    a++;
+// while (a<=11) {
+//     console.log(a);
+//     a++;
     
-}
+// }
 
-print 0 to 100 number
-let num =0 
-for (let num = 0; num <=100 ; num++) {
-    // const element = array[num];
-    console.log(num);
+// print 0 to 100 number
+// let num =0 
+// for (let num = 0; num <=100 ; num++) {
+//     // const element = array[num];
+//     console.log(num);
     
-}
+// }
 
-for (let num = 0; num <=100 ; num++) {
-    // const element = array[num];
-    if(num % 2 ===0){
-        console.log('your even num is:-',num);
-    }
+// for (let num = 0; num <=100 ; num++) {
+//     // const element = array[num];
+//     if(num % 2 ===0){
+//         console.log('your even num is:-',num);
+//     }
     
-}
+// }
 
 let gamenum = 30;
 let usernum = prompt("guess the game number")
