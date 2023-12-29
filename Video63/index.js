0100a76a0de1991ef7a0208711c1a6c1c311dad7
Ankitmaurya-1 <ankitmaurya9560@gmail.com>
2023-12-29 18:45:32 +0530

    Practices

---
+++ Video63/index.js
@@ -0,0 +1,38 @@
+// // looping in array
+// let heroes = ["Ankit","Ironman","Wanda","Batman","Spiderman","Hulk","Thor","Dr.Strange"]
+// for loop
+// for (let i = 0; i < heroes.length; i++) {
+//     // const element = array[heroes];
+//     console.log(heroes[i])
+// }
+
+// for of loop
+
+// for (let hero of heroes) {
+//     console.log(hero)
+// }
+
+// question
+// let marks = [85,97,44,38,76,60];
+// let sum =0;
+// for(let val of marks){
+//     sum+=val;
+
+// }
+// let avg = sum/marks.length
+// console.log(`Avg marks of calss is = ${avg}`)
+
+// let price = [250,645,300,900,50];
+// for (let i = 0; i < price.length; i++) {
+//     // const element = array[i];
+// let offer = price[i]/10;
+// price[i]-= offer;
+    
+// }
+// console.log(price);
+
+// let comp = ["Google","Microsoft","Amazon","Uber","Netflix","Apple"]
+// // comp.shift()
+// // comp.splice(3,1,"ola")
+// comp.push("IBM")
+// console.log(comp);
--- Video63/script.js
+++ Video63/script.js
@@ -1,24 +1,31 @@
- // Arrays are mutable and string are imutables
- let arr = [123,5,54,34,3]
- console.log(arr)
-console.log(arr.length)
+//  // Arrays are mutable and string are imutables
+//  let arr = [123,5,54,34,3]
+//  console.log(arr)
+// console.log(arr.length)
 
-console.log(typeof arr)
+// console.log(typeof arr)
 
-console.log(arr.toString())
-console.log(arr.join(" and "))
+// console.log(arr.toString())
+// console.log(arr.join(" and "))
 
-console.log(arr.pop()) // pop take out the last element from the origina array 
-console.log(arr)
+// console.log(arr.pop()) // pop take out the last element from the origina array 
+// console.log(arr)
 
- console.log(arr.push(10)) // push take add the new element to the last place of the arry
+//  console.log(arr.push(10)) // push take add the new element to the last place of the arry
 
- console.log(arr)
- console.log(arr.shift()) // take the first element form arry 
+//  console.log(arr)
+//  console.log(arr.shift()) // take the first element form arry 
 
- console.log(arr.unshift()) // add to the first element form arry 
+//  console.log(arr.unshift()) // add to the first element form arry 
 
- let a = [1,2,4,5,3,6,8,7]
- f
- console.log(a.splice(3,4)) // spilce take out the part from arry which indexes are passed into parameters in parameters first para is index form where you wanted to start splicing and end param is how many elements to take out after that index
- console.log(a.slice(2,4)) // pass the start:end indexes ; end index is exculsive
+//  let a = [1,2,4,5,3,6,8,7]
+//  f
+//  console.log(a.splice(3,4)) // spilce take out the part from arry which indexes are passed into parameters in parameters first para is index form where you wanted to start splicing and end param is how many elements to take out after that index
+//  console.log(a.slice(2,4)) // pass the start:end indexes ; end index is exculsive
+
+// Arrays 
+let marks = [89,89,8,689,78,678,8,7]
+console.log(marks)
+console.log(typeof marks)
+console.log(marks[7]=12)
+console.log(marks)
