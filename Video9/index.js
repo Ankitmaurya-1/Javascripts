2baf6d2e75de1037c5335752ad2332a220a564fd
Ankitmaurya-1 <ankitmaurya9560@gmail.com>
2024-01-05 17:34:36 +0530

    Practicing DOM

---
+++ Video9/index.js
@@ -0,0 +1,15 @@
+// practice problem ON Dom
+//Q1
+
+// let heading = document.querySelector("h2")
+// console.dir(heading.innerText)
+// heading.innerText = heading.innerText + " From Apna College"
+// console.dir(newheading)
+
+// Q2
+
+let div = document.querySelectorAll(".box");
+
+div[0].innerText= "HI";
+div[1].innerText= "HOW  ";
+div[2].innerText= "WHY";
