3f4184ae2c4c150b461d90c8026e709d6578129f
Ankitmaurya-1 <ankitmaurya9560@gmail.com>
2024-01-06 13:40:39 +0530

    Learning Dom part 2

---
+++ Video10/scirpt.js
@@ -0,0 +1,55 @@
+// let div = document.querySelector("div");
+// console.dir(div);
+
+// let attr = div.getAttribute("id")
+// console.dir(attr)
+
+// let div = document.querySelector("div");
+// console.dir(div);
+
+// div.style.height = "200px";
+// div.style.color = "white";
+// div.style.textDecoration = "underline";
+// div.style.fontSize = "17px";
+// div.innerText = "chup ho ja";
+// div.style.visibility = "Hidden"
+
+// let newbtn = document.createElement("button");
+// newbtn.innerText = "Click Me!";
+// console.dir(newbtn)
+
+// let div = document.querySelector("body");
+// div.before(newbtn);  
+
+// let para = document.createElement("p");
+// para.innerText="This is new para ! ";
+
+// let afdiv = document.querySelector("div");
+// afdiv.after(para)
+
+// let p = document.querySelector("p");
+// p.remove("p")
+
+// // pratice question
+
+// let btn = document.createElement("button");
+// btn.innerText = "click me";
+// btn.style.backgroundColor = "red";
+// btn.style.color = "white";
+
+// let body = document.querySelector("body");
+// body.prepend(btn);
+
+// let para = document.querySelector("p");
+// para.getAttribute("class")
+// para.classList.add("para2")
+
+
+
+
+
+
+
+
+
+
--- Video9/index.js
+++ Video9/index.js
@@ -8,8 +8,15 @@
 
 // Q2
 
-let div = document.querySelectorAll(".box");
+// let div = document.querySelectorAll(".box");
 
-div[0].innerText= "HI";
-div[1].innerText= "HOW  ";
-div[2].innerText= "WHY";
+// div[0].innerText= "HI";
+// div[1].innerText= "HOW  ";
+// div[2].innerText= "WHY";
+
+// let table = 19;
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${table} * ${i} = ${table*i} `)
+// }
+
+
