f46f65bd50d0730f3d1b0f9623d70fe2ba1bc71a
Ankit <ankitmaurya9560@gmail.com>
2024-01-20 21:24:00 +0530

    Class and Objects

--- Video14/script.js
+++ Video14/script.js
@@ -1,36 +1,75 @@
-// const employee = {
-//     calcTax() {
-//         console.log("TAX RATE IS 10%");
+// const students = {
+//     fullName: "Ankit Maurya",
+//     marks: 90,
+//     printMarks() {      // function in objects
+//         console.log(this.marks); // students.marks acces thorugh this
 //     }
 // };
 
-// const KaranArju = {
-//     Salary: 500000,
+// const employee = {
+//     calctax() {
+//         console.log("Tax rate is 10%");
+//     }
 // };
 
-// KaranArju.__proto__ = employee; // this will create 1st object a prototype in second object
+// const KaranArjun = {
+//     salary: 50000,
+// calctax() {
+//         console.log("Tax rate is 20%");  if object and prototype have same fucntion than object;s method will be used
+//     }
 
-class Car { // constructor is used to set the intial values of any objects beacause it invovked atuomatically at the first
-    constructor(brand, mileage, runned, modelYear, Color) {
-        console.log("Create new object");
-        this.brand = brand;
-        this.mileage = mileage;
-        this.runned = runned;
-        this.modelYear = modelYear;
-        this.Color = Color;
-    }
-    start() {
-        console.log("Car start !");
-    }
-    stop() {
-        console.log("Car stopped !");
-    }
+// };
 
-} // class are the templates to create many objects bascily they are tha intial values of any objects
+// KaranArjun.__proto__ = employee;
 
-let fourtuner = new Car("Toyota", 13, "3000km", 2019, "white");
-console.log(fourtuner);
-// fourtuner.setBrand("Toyota");/
+// class Car {
+//     constructor(brand, mileage, model) {
+//         this.brand = brand;
+//         this.mileage = mileage;
+//         this.model = model;
+//     }
+//     start() {
+//         console.log("Car start !");
+//     }
+//     stop() {
+//         console.log("Car stop !");
+//     }
+// }
 
-let urus = new Car("Lambo", 30, "5000km", 2022, "Yellow");
-console.log(urus);
+// let supra = new Car("toyota", 10, 2022);
+// let urus = new Car("lambo", 15, 2024);
+
+
+// class parent {
+//     hello() {
+//         console.log("Hello Dear !");
+//     }
+// };
+
+// class child extends parent { }
+
+// let obj1 = new child()
+
+// class user {
+//     constructor(name, email) {
+//         this.name = name;
+//         this.email = email;
+//     }
+
+//     viewData() {
+//         console.log("You have viewd website data");
+//     }
+// }
+// class admin extends user {
+//     constructor(name, email) {
+//         super(name,email);
+//     }
+//     editData() {
+//         console.log("you can now edit");
+//     }
+// }
+
+// let student1 = new user("Ankit", "ankit@gmail.com");
+// let student2 = new user("Ankit", "ankit@gmail.com");
+
+// let aadmin = new admin("admin", "admin@email.com");
